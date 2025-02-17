6112f73951af8de3edaffe9d9ecbc0e4e9e1a8cb
Verneri <verneri.lopperi@gmail.com>
2025-02-17 14:56:03 +0200

    feature: implement formik+yup validation for registerScreen and modify CustomUser

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     email = models.EmailField(
         error_messages={"unique": "A user with that email already exists."},
-        max_length=254,
+        max_length=128,
         verbose_name="email address",
         unique=True,
+    )
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[UnicodeUsernameValidator()],
+        error_messages={"unique": "A user with that username already exists."},
     )
 
     def __str__(self):
