1f3dec8ad36afd411ef6cbb42804ba09fe6746d7
Verneri <verneri.lopperi@gmail.com>
2025-05-15 11:13:15 +0300

    Feature: Add rkt queries for posts.

--- frontend/src/redux/api/endpoints/posts.ts
+++ frontend/src/redux/api/endpoints/posts.ts
@@ -1,31 +1,111 @@
 import {apiSlice} from '../apiSlice';
 
-import {LikeType, PostType} from '../../../types/types';
+import {
+  CommentDeleteRequestType,
+  CommentRequestType,
+  CommentType,
+  CommentTypeWithCount,
+  GetCommentsRequestType,
+  GetLikesRequestType,
+  LikeType,
+  LikeTypeWithCount,
+  PostRequestType,
+  PostType,
+  PostTypeWithCount,
+} from '../../../types/types';
 
 export const postsApi = apiSlice.injectEndpoints({
   endpoints: builder => ({
-    getFeedPosts: builder.query<PostType[], void>({
-      query: () => 'posts/',
+    getFeedPosts: builder.query<PostTypeWithCount, string>({
+      query: last_post_created_at =>
+        `posts/?last_post_created_at=${last_post_created_at}`,
       providesTags: () => [{type: 'Posts'}],
     }),
-
-    likeImage: builder.mutation<LikeType, number>({
-      query: image_id => ({
-        url: `images/${image_id}/likes/`,
+    createPost: builder.mutation<PostType, PostRequestType>({
+      query: ({image_url, thumbnail_url, caption, is_visible}) => ({
+        url: 'posts/',
         method: 'POST',
-        body: {image_id},
+        body: {image_url, thumbnail_url, caption, is_visible},
       }),
-      invalidatesTags: (result, error, image_id) => [
-        {type: 'Likes', id: image_id},
+    }),
+    getPostById: builder.query<PostType, number>({
+      query: post_id => `posts/${post_id}/`,
+      providesTags: (result, error, post_id) => [{type: 'Posts', id: post_id}],
+    }),
+    updatePost: builder.mutation<
+      PostType,
+      {post_id: number} & Partial<PostRequestType>
+    >({
+      query: ({post_id, ...body}) => ({
+        url: `posts/${post_id}/`,
+        method: 'PATCH',
+        body,
+      }),
+      invalidatesTags: (result, error, {post_id}) => [
+        {type: 'Posts', id: post_id},
       ],
     }),
-    unLikeImage: builder.mutation<void, LikeType>({
-      query: ({id, image_id}) => ({
-        url: `images/${image_id}/likes/${id}`,
+    deletePost: builder.mutation<string, number>({
+      query: post_id => ({
+        url: `posts/${post_id}/`,
         method: 'DELETE',
       }),
-      invalidatesTags: (result, error, {image_id}) => [
-        {type: 'Likes', id: image_id},
+      invalidatesTags: (result, error, post_id) => [
+        {type: 'Posts', id: post_id},
+      ],
+    }),
+    getPostComments: builder.query<
+      CommentTypeWithCount,
+      GetCommentsRequestType
+    >({
+      query: ({post_id, last_comment_created_at, created_at}) =>
+        `posts/${post_id}/comments?last_comment_created_at=${last_comment_created_at}&order_by=${created_at}`,
+      providesTags: (result, error, {post_id}) => [
+        {type: 'Posts', id: post_id},
+      ],
+    }),
+    createComment: builder.mutation<CommentType, CommentRequestType>({
+      query: ({post_id, content}) => ({
+        url: `posts/${post_id}/comments`,
+        method: 'POST',
+        body: {content},
+      }),
+      invalidatesTags: (result, error, {post_id}) => [
+        {type: 'Posts', id: post_id},
+      ],
+    }),
+    deleteComment: builder.mutation<string, CommentDeleteRequestType>({
+      query: ({post_id, comment_id}) => ({
+        url: `posts/${post_id}/comments/${comment_id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, {post_id}) => [
+        {type: 'Posts', id: post_id},
+      ],
+    }),
+    getLikes: builder.query<LikeTypeWithCount, GetLikesRequestType>({
+      query: ({post_id, last_like_created_at}) =>
+        `posts/${post_id}/likes?last_like_created_at=${last_like_created_at}`,
+      providesTags: (result, error, {post_id}) => [
+        {type: 'Posts', id: post_id},
+      ],
+    }),
+    addLike: builder.mutation<LikeType, number>({
+      query: post_id => ({
+        url: `posts/${post_id}/likes`,
+        method: 'POST',
+      }),
+      invalidatesTags: (result, error, post_id) => [
+        {type: 'Posts', id: post_id},
+      ],
+    }),
+    deleteLike: builder.mutation<string, number>({
+      query: post_id => ({
+        url: `posts/${post_id}/comments/`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, post_id) => [
+        {type: 'Posts', id: post_id},
       ],
     }),
   }),
@@ -33,6 +113,14 @@ export const postsApi = apiSlice.injectEndpoints({
 
 export const {
   useGetFeedPostsQuery,
-  useLikeImageMutation,
-  useUnLikeImageMutation,
+  useCreatePostMutation,
+  useGetPostByIdQuery,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+  useGetPostCommentsQuery,
+  useCreateCommentMutation,
+  useDeleteCommentMutation,
+  useGetLikesQuery,
+  useAddLikeMutation,
+  useDeleteLikeMutation,
 } = postsApi;
