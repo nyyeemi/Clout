d5b304f17f51f819ffea8a3dada0ef4a56b38b29
Nyyssönen Eemil <eemil.nyyssonen@gmail.com>
2026-01-14 15:46:57 +0200

    feature: sketch initial component structure for leaderboard screen

--- frontend/src/screens/LeaderboardScreen/LeaderboardScreen.tsx
+++ frontend/src/screens/LeaderboardScreen/LeaderboardScreen.tsx
@@ -1,15 +1,16 @@
 import React, {useCallback} from 'react';
+import {Image, StyleSheet, View} from 'react-native';
 
 import {useFocusEffect} from '@react-navigation/native';
+import {FlashList} from '@shopify/flash-list';
 import Toast from 'react-native-toast-message';
 
 import globalStyle from '../../assets/styles/globalStyle';
 import {ThemedView} from '../../components/ui/themed-view';
-import {ThemedText} from '../../components/ui/typography';
 
 // adjust if needed
 
-export const LeaderboardScreen = (): JSX.Element => {
+export const LeaderboardScreen = () => {
   useFocusEffect(
     useCallback(() => {
       Toast.show({
@@ -19,10 +20,56 @@ export const LeaderboardScreen = (): JSX.Element => {
     }, []),
   );
 
+  const leaderboardData = [];
+  const renderItem = useCallback(() => <LeaderboardItem />, []);
+
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <ThemedView style={[globalStyle.flex, {justifyContent: 'center'}]}>
-      <ThemedText>moromoro tää on leaderboardscreen</ThemedText>
+      <PodiumView />
+      {/*<FlashList
+        data={[leaderboardData]}
+        ListHeaderComponent={<PodiumView />}
+        renderItem={renderItem}
+      />*/}
     </ThemedView>
   );
 };
+
+type PodiumViewProps = {};
+
+const PodiumView = () => {
+  return (
+    <View style={styles.podiumContainer}>
+      <Image
+        source={{
+          uri: 'https://picsum.photos/seed/9cf2603f-852c-5321-a393-5c9885083598_post_2/400/300',
+        }}
+      />
+      <Image
+        source={{
+          uri: 'https://picsum.photos/seed/9cf2603f-852c-5321-a393-5c9885083598_post_2/400/300',
+        }}
+      />
+      <Image
+        source={{
+          uri: 'https://picsum.photos/seed/9cf2603f-852c-5321-a393-5c9885083598_post_2/400/300',
+        }}
+      />
+    </View>
+  );
+};
+
+type LeaderBoardProps = {};
+
+const LeaderboardItem = () => {
+  return <></>;
+};
+
+const styles = StyleSheet.create({
+  leaderboardItemContainer: {},
+  podiumContainer: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+});
